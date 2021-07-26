0964fa17d66a795b04c6622941b7a393f6838137
Bill <billfeng2002@gmail.com>
2021-07-25 18:05:02 -0700

    commit tree 3506fff909c3985ccc38194e5d4f33789509676d

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+let driver={}
+
+function updateDriverWithKeyAndValue(driver, key, value){
+    let newD={...driver}
+    newD[key]=value
+    return newD
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    driver[key]=value
+    return driver
+}
+
+function deleteFromDriverByKey(driver, key){
+    let newD={...driver}
+    delete newD[key]
+    return newD
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+    delete driver[key]
+    return driver
+}
